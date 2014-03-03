43d53f1eefe84e980ff1a075dec4b3404eb4159f
Mathew Winstone <mwinstone@coldfrontlabs.ca>
2014-03-03 16:32:04 -0500

    Added default drush version

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,6 +1,12 @@
 class drush ($version = $::version) {
   include composer
 
+  if ($version) {
+    # Do nothing
+  } else {
+    $version = '6.x'
+  }
+
   class {'composer':
     provider => 'drush/drush:${version}',
     ensure => present,
