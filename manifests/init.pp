6abd67752a1f88f066d3fe1cd0a9935aa3b61158
Mathew Winstone <mwinstone@coldfrontlabs.ca>
2014-03-04 15:01:37 -0500

    Changed to project_name

--- manifests/init.pp
+++ manifests/init.pp
@@ -2,7 +2,7 @@ class drush ($version = $::version) {
   include composer
 
   composer::requirepack {'drush/drush:${version}':
-      package_name => 'drush/drush:6.*',
+      project_name => 'drush/drush:6.*',
       cwd                  => '/usr/local/bin', # REQUIRED
       global               => true,  # Add global requirement
   }
