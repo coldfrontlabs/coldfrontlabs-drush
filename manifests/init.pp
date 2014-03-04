023e89f9bc18c41f19862b0914b997f12bb19d85
Mathew Winstone <mwinstone@coldfrontlabs.ca>
2014-03-04 14:50:13 -0500

    removed all except what's required

--- manifests/init.pp
+++ manifests/init.pp
@@ -4,13 +4,5 @@ class drush ($version = $::version) {
   composer::requirepack {'drush/drush:${version}':
       cwd                  => '/usr/local/bin', # REQUIRED
       global               => true,  # Add global requirement
-      prefer_source        => false,
-      prefer_dist          => false,
-      dry_run              => false, # Just simulate actions
-      custom_installers    => false, # No custom installers
-      scripts              => false, # No script execution
-      interaction          => false, # No interactive questions
-      optimize             => false, # Optimize autoloader
-      dev                  => false, # Install dev dependencies
   }
 }
