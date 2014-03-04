26d83db48b44149ddbc518b45af7d7c590a948c5
Mathew Winstone <mwinstone@coldfrontlabs.ca>
2014-03-04 15:16:12 -0500

    Trying a new way

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,9 +1,22 @@
 class drush ($version = $::version) {
-  include composer
+  class { 'composer':
+      target_dir      => '/usr/local/bin',
+      composer_file   => 'composer', # could also be 'composer.phar'
+      download_method => 'curl',     # or 'wget'
+      logoutput       => false,
+      tmp_path        => '/tmp',
+      php_package     => 'php5-cli',
+      curl_package    => 'curl',
+      wget_package    => 'wget',
+      composer_home   => '/root',
+      php_bin         => 'php', # could also i.e. be 'php -d "apc.enable_cli=0"' for more fine grained control
+      suhosin_enabled => true,
+  }
 
-  composer::requirepack {'drush/drush:${version}':
+
+  composer::require {'drush/drush:${version}':
       project_name => 'drush/drush:6.*',
-      cwd                  => '/usr/local/bin', # REQUIRED
+      target_dir                  => '/usr/local/bin', # REQUIRED
       global               => true,  # Add global requirement
   }
 }
