27a31697c2b38f78c422fff98c569dc9f6ec2b58
Mathew Winstone <mwinstone@coldfrontlabs.ca>
2014-03-05 16:30:18 -0500

    Added drush si template

---
+++ manifests/si.pp
@@ -0,0 +1,13 @@
+define drush::si ($profile = undef, $settings = undef, $options = undef) {
+
+  exec {"drush-si-${name}":
+    command => "drush si $profile $settings -y",
+    cwd     => $sitepath,
+  }
+
+  if defined(Exec["drush-si-${name}"]) {
+    Exec["drush-si-${name}"] {
+      before  => Exec["drush-si-${name}"],
+    }
+  }
+}
