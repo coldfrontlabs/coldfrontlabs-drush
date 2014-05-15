dec561b895d9db983b4783193507c5bd37a9c8f5
Mathew Winstone <mwinstone@coldfrontlabs.ca>
2014-05-15 12:30:30 -0400

    Added drush dl

--- manifests/dl.pp
+++ manifests/dl.pp
@@ -1,4 +1,4 @@
-define drush::dl ($destination = undef, $source = undef, $project_name = undef) {
+define drush::dl ($destination = undef, $source = undef, $project_name = undef, $default_major = undef) {
 
   if !$destination {
     $dst = ""
@@ -16,8 +16,14 @@ define drush::dl ($destination = undef, $source = undef, $project_name = undef) 
     $project_name = $name
   }
 
+  if !$default_major {
+    $dm = ""
+  } else {
+    $dm = "--default_major=$default_major"
+  }
+
   exec {"drush-dl-${name}":
-    command => "drush $dst $src dl ${project_name} -y",
+    command => "drush $dst $src $dm dl ${project_name} -y",
     cwd     => $sitepath,
   }
 
@@ -27,3 +33,54 @@ define drush::dl ($destination = undef, $source = undef, $project_name = undef) 
     }
   }
 }
+
+
+# -cache                                   Cache release XML and tarballs
+#                                           or git clones. Git clones use
+#                                           git's --reference option.
+#   --cache-duration-releasexml             Expire duration (in seconds) for
+#                                           release XML. Defaults to 86400
+#                                           (24 hours).
+# --default-major=<6>                       Specify the default major
+#                                           version of modules to download
+#                                           when there is no bootstrapped
+#                                           Drupal site.  Defaults to "7".
+# --destination=<path>                      Path to which the project will
+#                                           be copied. If you're providing a
+#                                           relative path, note it is
+#                                           relative to the drupal root (if
+#                                           bootstrapped).
+# --dev                                     Work with development releases
+#                                           solely.
+# --drupal-project-rename                   Alternate name for "drupal-x.y"
+#                                           directory when downloading
+#                                           Drupal project. Defaults to
+#                                           "drupal".
+# --notes                                   Show release notes after each
+#                                           project is downloaded.
+# --pipe                                    Returns a list of the names of
+#                                           the extensions (modules and
+#                                           themes) contained in the
+#                                           downloaded projects.
+# --select                                  Select the version to download
+#                                           interactively from a list of
+#                                           available releases.
+#   --all                                   Shows all available releases
+#                                           instead of a short list of
+#                                           recent releases.
+# --skip                                    Skip automatic downloading of
+#                                           libraries (c.f. devel).
+# --source                                  The base URL which provides
+#                                           project release history in XML.
+#                                           Defaults to
+#                                           http://updates.drupal.org/releas
+#                                           e-history.
+# --use-site-dir                            Force to use the site specific
+#                                           directory. It will create the
+#                                           directory if it doesn't exist.
+#                                           If --destination is also present
+#                                           this option will be ignored.
+# --variant=<full>                          Only useful for install
+#                                           profiles. Possible values:
+#                                           'full', 'projects',
+#                                           'profile-only'.
