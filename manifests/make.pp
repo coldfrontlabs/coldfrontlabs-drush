659b9280c1f96cc62d8f0c753367d995b3db0ab7
Mathew Winstone <mwinstone@coldfrontlabs.ca>
2014-05-16 19:48:36 -0400

    Changed the test yet again

--- manifests/make.pp
+++ manifests/make.pp
@@ -110,7 +110,7 @@ define drush::make ($makefile,
   exec { "drush-make-rmdir-${makefile}-${build_path}":
     command  => "rmdir ${build_path} &> /dev/null || true",
     # Check if the path is empty or if the directory exists that it is empty
-    onlyif => "test -d ${build_path} -a -z `ls -A ${build_path} | wc -l` -o ! -e ${build_path}"
+    onlyif => "test -d ${build_path} -a -z `ls -A ${build_path} | wc -l` || test ! -e ${build_path}"
   }
 
   # Since drush make won't overwrite an existing directory, we remove an empty directory if it's there already (say for instance puppet/apache already provisioned it)
