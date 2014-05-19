4d2c42a8a1d64162640dc174386bc35c9de1adc6
Mathew Winstone <mwinstone@coldfrontlabs.ca>
2014-05-18 22:02:57 -0400

    Added relative path

--- manifests/init.pp
+++ manifests/init.pp
@@ -23,7 +23,7 @@ class drush ($version = $::version) {
       require => File['/tmp/drushme/composer.json'],
     }->
     exec {'drush-env-refresh':
-      command => 'source .bashrc',
+      command => 'source ~/.bashrc',
     }->
     exec {'drush-status-check':
       command => 'drush status',
