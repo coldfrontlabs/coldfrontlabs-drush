d2d5eb9375a6463614a9a76170c596c0ec5fbb41
Mathew Winstone <mwinstone@coldfrontlabs.ca>
2014-05-26 16:09:52 -0400

    Added composer home

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,11 +1,21 @@
 class drush ($version = $::version) {
-  Exec { path => [ "/bin/", "/sbin/" , "/usr/bin/", "/usr/sbin/", "/usr/local/bin", "/usr/local/sbin", "${::composer_home}/vendor/bin" ] }
+  Exec { path => [ "/bin/", "/sbin/" , "/usr/bin/", "/usr/sbin/", "/usr/local/bin", "/usr/local/sbin" ] }
   package { ['zip', 'unzip']: ensure => present}
+
+  file {'/usr/local/share/composer':
+    ensure => 'directory',
+  }
+
+  class { 'composer':
+    composer_home   => '/usr/local/share/composer',
+    require         => File['/usr/local/share/composer'],
+  }
 
   composer::require {"drush_global":
     project_name => 'drush/drush',
     global => true,
     version => "6.*",
+    require => Class['composer'],
   }
 
   file {"/usr/bin/drush":
