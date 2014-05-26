0509a9558feca76cd502fbf975488a1bfc4ba758
Mathew Winstone <mwinstone@coldfrontlabs.ca>
2014-05-26 13:41:57 -0400

    Changed to composer require

--- manifests/init.pp
+++ manifests/init.pp
@@ -2,31 +2,23 @@ class drush ($version = $::version) {
   Exec { path => [ "/bin/", "/sbin/" , "/usr/bin/", "/usr/sbin/", "/usr/local/bin", "/usr/local/sbin" ] }
   package { ['zip', 'unzip']: ensure => present}
 
-#  if $hasdrush == 'not-installed' {
     include composer
 
-    file {'/tmp/drushme':
-      mode => '0755',
-      ensure => 'directory'
-    }
+#    file {'/tmp/drushme':
+#      mode => '0755',
+#      ensure => 'directory'
+#    }
 
-    file {'/tmp/drushme/composer.json':
-      content => '{"name":"drushme","require":{"php":">=5.3.0"},"config":{"bin-dir":"/usr/local/bin","vendor-dir":"/usr/local/share/composer"},"require":{"drush/drush":">=6"}}',
-      require => File['/tmp/drushme'],
-      mode => '0644',
-      ensure => 'present',
+    composer::require {"drush_global":
+      project_name => 'drush/drush',
+      global => true,
+      version => "6.*",
     }
-
-    composer::exec {'install-drush':
-      cmd => 'install',
-      cwd => '/tmp/drushme',
-      require => File['/tmp/drushme/composer.json'],
-    }->
-    exec {'drush-env-refresh':
-      command => 'bash',
-    }->
-    exec {'drush-status-check':
-      command => 'drush status',
-    }
-#  }
+#    ->
+#    exec {'drush-env-refresh':
+#      command => 'bash',
+#    }->
+#    exec {'drush-status-check':
+#      command => 'drush status',
+#    }
 }
