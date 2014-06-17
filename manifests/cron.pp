ea22b424e2060ec34545470e7eaf7abf85f29263
Mathew Winstone <mwinstone@coldfrontlabs.ca>
2014-06-17 09:43:06 -0400

    Added cron command

---
+++ manifests/cron.pp
@@ -0,0 +1,13 @@
+define drush::cron ($site_root) {
+
+  Exec { path => [ "/bin/", "/sbin/" , "/usr/bin/", "/usr/sbin/", "/usr/local/bin", "/usr/local/sbin" ] }
+
+	# Build the arguments to the command.
+	$root_arg = "--root=$site_root"
+
+  exec {"drush-si-${name}":
+    command => "drush $root_arg cron -y",
+    cwd     => $site_root,
+    timeout => 0,
+  }
+}
