f9d561b470d90e077cd0fbe7f1bbcfa526f84766
Mathew Winstone <mwinstone@coldfrontlabs.ca>
2014-06-17 09:46:02 -0400

    Added conditional

--- manifests/cron.pp
+++ manifests/cron.pp
@@ -3,7 +3,9 @@ define drush::cron ($site_root) {
   Exec { path => [ "/bin/", "/sbin/" , "/usr/bin/", "/usr/sbin/", "/usr/local/bin", "/usr/local/sbin" ] }
 
 	# Build the arguments to the command.
-	$root_arg = "--root=$site_root"
+	if $site_root {
+    $root_arg = "--root=${site_root}"
+  }
 
   exec {"drush-si-${name}":
     command => "drush $root_arg cron -y",
