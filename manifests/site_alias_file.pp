77fbd146aff2def062f2b6d37c559b127904cff5
Mathew Winstone <mwinstone@coldfrontlabs.ca>
2014-07-30 18:39:00 -0400

    Testing some variables

--- manifests/site_alias_file.pp
+++ manifests/site_alias_file.pp
@@ -1,5 +1,5 @@
 # Add an instance of an *.aliases.drushrc.php file
-define drush::site_alias_file($aliases) {
+define drush::site_alias_file($aliases, $uri = undef, $remote_host = undef, $remote_user = undef, $os = undef) {
 
   file {"/etc/drush/${name}.aliases.drushrc.php":
     ensure => 'file',
