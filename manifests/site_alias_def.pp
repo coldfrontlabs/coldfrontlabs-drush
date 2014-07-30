ee81c4ccb7abb3a5d75b7236024ae13f39ef8666
Mathew Winstone <mwinstone@coldfrontlabs.ca>
2014-07-30 19:46:45 -0400

    Added db url

--- manifests/site_alias_def.pp
+++ manifests/site_alias_def.pp
@@ -1,5 +1,5 @@
 # Add an instance of an *.aliases.drushrc.php file
-define drush::site_alias_def($name, $group, $uri = undef, $remote_user = undef, $remote_host = undef, $os = undef) {
+define drush::site_alias_def($name, $group, $uri = undef, $remote_user = undef, $remote_host = undef, $os = undef, $db_url = undef) {
 
   concat::fragment {"drush-site-alias-def-{$name}-{$group}":
     target => "/etc/drush/${group}.aliases.drushrc.php",
