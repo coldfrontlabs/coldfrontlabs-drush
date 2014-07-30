8eaa991bddd5f29189586cbe33178579b0ddd618
Mathew Winstone <mwinstone@coldfrontlabs.ca>
2014-07-30 19:55:08 -0400

    Fixed php template

--- manifests/site_alias_def.pp
+++ manifests/site_alias_def.pp
@@ -1,12 +1,5 @@
 # Add an instance of an *.aliases.drushrc.php file
 define drush::site_alias_def($group, $uri = undef, $remote_user = undef, $remote_host = undef, $os = undef, $db_url = undef) {
 
-  concat::fragment {"drush-site-alias-def-{$name}-{$group}":
-    target => "/etc/drush/${group}.aliases.drushrc.php",
-    content => template('drush/aliases.drushrc.erb'),
-    require => [
-      File['/etc/drush'],
-      Drush::Site_alias_group["${group}"],
-    ]
-  }
+
 }
--- manifests/site_alias_group.pp
+++ manifests/site_alias_group.pp
@@ -1,5 +1,5 @@
 # Add an instance of an *.aliases.drushrc.php file
-define drush::site_alias_group($name, $description = undef) {
+define drush::site_alias_group($name, $aliases = undef, $description = undef) {
 
   file {"/etc/drush/${name}.aliases.drushrc.php":
     ensure => 'file',
@@ -7,6 +7,14 @@ define drush::site_alias_group($name, $description = undef) {
     group => 'root',
     mode => '0644',
     content => template('drush/php.erb'),
+    require => [
+      File['/etc/drush'],
+    ]
+  }
+
+  concat::fragment {"drush-site-alias-{$name}":
+    target => "/etc/drush/${name}.aliases.drushrc.php",
+    content => template('drush/aliases.drushrc.erb'),
     require => [
       File['/etc/drush'],
     ]
