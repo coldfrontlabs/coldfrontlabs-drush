d186d2d4a12fd68b4dadeeffb78e8bc9f851143a
Mathew Winstone <mwinstone@coldfrontlabs.ca>
2014-07-30 19:44:21 -0400

    Fixed name

--- manifests/site_alias_group.pp
+++ manifests/site_alias_group.pp
@@ -1,5 +1,5 @@
 # Add an instance of an *.aliases.drushrc.php file
-define drush::site_alias_file() {
+define drush::site_alias_group($name, $description = undef) {
 
   file {"/etc/drush/${name}.aliases.drushrc.php":
     ensure => 'file',
