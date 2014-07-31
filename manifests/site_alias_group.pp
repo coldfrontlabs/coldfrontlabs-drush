53982afd2d98abc72519f0a75adc387140e1a55d
Mathew Winstone <mwinstone@coldfrontlabs.ca>
2014-07-31 10:01:29 -0400

    Added fragments

--- manifests/site_alias_group.pp
+++ manifests/site_alias_group.pp
@@ -12,9 +12,25 @@ define drush::site_alias_group($name, $aliases = undef, $description = undef) {
     ]
   }
 
+  concat {"/etc/drush/${name}.aliases.drushrc.php":
+    owner => 'root',
+    group => 'root',
+    mode => '0644',
+  }
+
+  concat::fragment {"drush-site-alias-sig-{$name}":
+    target => "/etc/drush/${name}.aliases.drushrc.php",
+    content => template('drush/php.erb'),
+    order => 1,
+    require => [
+      File['/etc/drush'],
+    ],
+  }
+
   concat::fragment {"drush-site-alias-{$name}":
     target => "/etc/drush/${name}.aliases.drushrc.php",
     content => template('drush/aliases.drushrc.erb'),
+    order => 2,
     require => [
       File['/etc/drush'],
     ]
