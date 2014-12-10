cfd53762f33cb7d02eec42239bd75c3a4126ef9c
Mathew Winstone <mwinstone@coldfrontlabs.ca>
2014-12-10 08:47:11 -0500

    Fix cron to not require a site root

--- manifests/cron.pp
+++ manifests/cron.pp
@@ -1,15 +1,14 @@
-define drush::cron ($site_root) {
+define drush::cron ($site_root = undef) {
 
   Exec { path => [ "/bin/", "/sbin/" , "/usr/bin/", "/usr/sbin/", "/usr/local/bin", "/usr/local/sbin" ] }
 
 	# Build the arguments to the command.
 	if $site_root {
-    $root_arg = "--root=${site_root}"
+    $siteroot = "--root=${site_root}"
   }
 
   exec {"drush-si-${name}":
-    command => "drush $root_arg cron -y",
-    cwd     => $site_root,
+    command => "drush $siteroot cron -y",
     timeout => 0,
   }
 }
