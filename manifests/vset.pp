a5143aa6c3843a52fef7b2f3db448e92761f04ed
Mathew Winstone <mwinstone@coldfrontlabs.ca>
2015-01-19 18:11:08 -0500

    Add vset and vget Needs testing

--- manifests/vset.pp
+++ manifests/vset.pp
@@ -1,5 +1,5 @@
-# Drush Variable Get
-define drush::vget ($variable = undef,
+define drush::vset ($variable,
+                  $value,
                   $exact = false,
                   $format = undef,
                   $pipe = false,
@@ -8,11 +8,12 @@ define drush::vget ($variable = undef,
                   $onlyif = 'test !'
                 )
 {
-
   Exec { path => [ "/bin/", "/sbin/" , "/usr/bin/", "/usr/sbin/", "/usr/local/bin", "/usr/local/sbin" ] }
 
-  if $variable {
-    $v = "${variable}"
+	# Build the arguments for the command.
+	if $site_root {
+    validate_absolute_path($site_root)
+    $siteroot = "--root=${site_root}"
   }
 
   if $format {
@@ -27,24 +28,15 @@ define drush::vget ($variable = undef,
     $p = "--pipe"
   }
 
-	if $site_root {
-    validate_absolute_path($site_root)
-    $siteroot = "--root=${site_root}"
-  }
-
 	if $uri {
     $u = "--l=${uri}"
   }
 
   $hash = md5("$site_root-$uri-$variable-$value")
 
-  exec {"drush-vget-${hash}":
-    command => "drush vget $v $f $e $p $siteroot $u -y",
-    cwd     => $site_root,
-    onlyif  => $onlyif,
-    timeout => 0,
-    require => [
-      Exec['drush_status_check'],
-    ]
+  exec {"drush-vset-${hash}":
+    command => "drush vset $variable $value $f $e $p $siteroot $u -y",
+    onlyif => $onlyif,
+    require => Exec['drush_status_check'],
   }
 }
