3ee659862f92ea8eb16228ccceded6be274bbad6
Mathew Winstone <mwinstone@coldfrontlabs.ca>
2015-01-29 08:50:34 -0500

    Simplified site alias group again

--- manifests/site_alias_group.pp
+++ manifests/site_alias_group.pp
@@ -1,16 +1,11 @@
 # Add an instance of an *.aliases.drushrc.php file
 define drush::site_alias_group($name, $aliases = undef, $description = undef) {
 
-  concat {"/etc/drush/${name}.aliases.drushrc.php":
+  file {"/etc/drush/${name}.aliases.drushrc.php":
     owner => 'root',
     group => 'root',
     mode => '0644',
-  }
-
-  concat::fragment {"drush-site-alias-{$name}":
-    target => "/etc/drush/${name}.aliases.drushrc.php",
     content => template('drush/php.erb','drush/aliases.drushrc.erb'),
-    order => 1,
     require => [
       File['/etc/drush'],
     ]
