6bf0d7d16a898a59b9e7018ee5c8b4f70d4cf870
Mathew Winstone <mwinstone@coldfrontlabs.ca>
2015-01-29 08:41:15 -0500

    Simplified the site alias template definition

--- manifests/site_alias_group.pp
+++ manifests/site_alias_group.pp
@@ -7,28 +7,12 @@ define drush::site_alias_group($name, $aliases = undef, $description = undef) {
     mode => '0644',
   }
 
-  concat::fragment {"drush-site-alias-sig-{$name}":
-    target => "/etc/drush/${name}.aliases.drushrc.php",
-    content => template('drush/php.erb'),
-    order => 1,
-    require => [
-      File['/etc/drush'],
-    ],
-  }
-
   concat::fragment {"drush-site-alias-{$name}":
     target => "/etc/drush/${name}.aliases.drushrc.php",
-    content => template('drush/aliases.drushrc.erb'),
-    order => 2,
+    content => template('drush/php.erb','drush/aliases.drushrc.erb'),
+    order => 1,
     require => [
       File['/etc/drush'],
     ]
   }
 }
-
-#  <% if uri != "" %>'uri' => '<%= uri %>',<% end %>
-#  <% if db_url != "" %>'db-url' => '<%= db_url %>',<% end %>
-#  <% if remote_host != "" %>'remote-host' => '<%= remote_host %>',<% end %>
-#  <% if remote_user != "" %>'remote-user' => '<%= remote_user %>',<% end %>
-#  <% if os != "" %>'os' => '<%= os %>',<% end %>
-#
