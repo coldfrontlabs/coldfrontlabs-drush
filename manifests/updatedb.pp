10beef518e55043f51742b87ab3fe58a738d3a8c
Mathew Winstone <mwinstone@coldfrontlabs.ca>
2015-02-11 10:22:22 -0500

    feat(updb): Add support for update-db

--- manifests/updatedb.pp
+++ manifests/updatedb.pp
@@ -1,11 +1,29 @@
-define drush::updatedb () {
+define drush::updatedb ($uri = undef,
+                  $site_root = undef,
+                  $onlyif = 'test !',
+                 )
+{
 
   Exec { path => [ "/bin/", "/sbin/" , "/usr/bin/", "/usr/sbin/", "/usr/local/bin", "/usr/local/sbin" ] }
 
-  drush::cc {"drush-cc-for-updatedb-${name}":args => 'drush'}
-  ->exec {"drush-updatedb":
-    command     => "drush cc drush && drush updatedb -y",
-    cwd         => $sitepath,
-    require     => Exec['drush_status_check'],
+	# Build the arguments for the command.
+	if $site_root {
+    validate_absolute_path($site_root)
+    $siteroot = "--root=${site_root}"
+  }
+
+	# Build the arguments to the command.
+	if $uri {
+    $u = "--l=${uri}"
+  }
+
+  exec {"drush-updb-${name}":
+    command => "drush updb $siteroot $u -y",
+    cwd     => $site_root,
+    onlyif  => $onlyif,
+    timeout => 0,
+    require => [
+      Exec['drush_status_check'],
+    ]
   }
 }
