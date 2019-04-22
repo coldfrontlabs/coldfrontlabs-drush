64746b898edbeac24cc0ee2e65cab4810e073ada
Mathew Winstone <mwinstone@coldfrontlabs.ca>
2019-04-22 11:00:12 -0400

    fix(puppet5) $name is implicit

--- manifests/site_alias_group.pp
+++ manifests/site_alias_group.pp
@@ -1,5 +1,5 @@
 # Add an instance of an *.aliases.drushrc.php file
-define drush::site_alias_group($name, $aliases = undef, $description = undef) {
+define drush::site_alias_group($aliases = undef, $description = undef) {
 
   file {"/etc/drush/${name}.aliases.drushrc.php":
     owner => 'root',
